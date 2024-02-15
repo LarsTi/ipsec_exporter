3490145110ff9db82af3e35e07098467686e4e16
Owen Schwartz <oschwartz10612@gmail.com>
2024-02-15 18:02:29 -0500

    Fix messages() issue

--- app/main.go
+++ app/main.go
@@ -14,35 +14,34 @@ func main() {
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatalln(http.ListenAndServe(":9814", nil))
 }
-func listSAs()([]LoadedIKE, error){
-	s, err := vici.NewSession()
-	if err != nil {
-		log.Printf("Error Connecting to vici: %s", err)
-		return nil, err
-	}
-	defer s.Close()
+func listSAs() ([]LoadedIKE, error) {
+    s, err := vici.NewSession()
+    if err != nil {
+        log.Printf("Error Connecting to vici: %s", err)
+        return nil, err
+    }
+    defer s.Close()
 
-	var retVar []LoadedIKE
-	msgs, err := s.StreamedCommandRequest("list-sas", "list-sa", nil)
-	if err != nil {
-		return retVar, err
-	}
-	for _,m := range msgs.Messages() {
-		if e := m.Err(); e != nil{
-			//ignoring this error
-			continue
-		}
-		for _, k := range m.Keys() {
-			inbound := m.Get(k).(*vici.Message)
-			var ike LoadedIKE
-			if e := vici.UnmarshalMessage(inbound, &ike); e != nil {
-				//ignoring this marshal/unmarshal errro!
-				continue
-			}
-			ike.Name = k
-			retVar = append(retVar, ike)
-
-		}
-	}
-	return retVar, nil
+    var retVar []LoadedIKE
+    msgs, err := s.StreamedCommandRequest("list-sas", "list-sa", nil)
+    if err != nil {
+        return retVar, err
+    }
+    for _, m := range msgs { // <- Directly iterate over msgs
+        if e := m.Err(); e != nil {
+            //ignoring this error
+            continue
+        }
+        for _, k := range m.Keys() {
+            inbound := m.Get(k).(*vici.Message)
+            var ike LoadedIKE
+            if e := vici.UnmarshalMessage(inbound, &ike); e != nil {
+                //ignoring this marshal/unmarshal error!
+                continue
+            }
+            ike.Name = k
+            retVar = append(retVar, ike)
+        }
+    }
+    return retVar, nil
 }
