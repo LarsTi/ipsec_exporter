0dbf679dbfcd670935171ed5aad0fb80ec0235a2
Owen Schwartz <oschwartz10612@gmail.com>
2024-02-16 10:15:41 -0500

    Close socket

--- app/main.go
+++ app/main.go
@@ -1,10 +1,11 @@
 package main
 
-import(
+import (
 	"log"
-	"github.com/strongswan/govici/vici"
 	"net/http"
+
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/strongswan/govici/vici"
 )
 
 func main() {
@@ -15,33 +16,37 @@ func main() {
 	log.Fatalln(http.ListenAndServe(":9814", nil))
 }
 func listSAs() ([]LoadedIKE, error) {
-    s, err := vici.NewSession()
-    if err != nil {
-        log.Printf("Error Connecting to vici: %s", err)
-        return nil, err
-    }
-    defer s.Close()
+	s, err := vici.NewSession()
+	if err != nil {
+		log.Printf("Error Connecting to vici: %s", err)
+		return nil, err
+	}
+	defer s.Close()
 
-    var retVar []LoadedIKE
-    msgs, err := s.StreamedCommandRequest("list-sas", "list-sa", nil)
-    if err != nil {
-        return retVar, err
-    }
-    for _, m := range msgs { // <- Directly iterate over msgs
-        if e := m.Err(); e != nil {
-            //ignoring this error
-            continue
-        }
-        for _, k := range m.Keys() {
-            inbound := m.Get(k).(*vici.Message)
-            var ike LoadedIKE
-            if e := vici.UnmarshalMessage(inbound, &ike); e != nil {
-                //ignoring this marshal/unmarshal error!
-                continue
-            }
-            ike.Name = k
-            retVar = append(retVar, ike)
-        }
-    }
-    return retVar, nil
+	var retVar []LoadedIKE
+	msgs, err := s.StreamedCommandRequest("list-sas", "list-sa", nil)
+	if err != nil {
+		return retVar, err
+	}
+	for _, m := range msgs { // <- Directly iterate over msgs
+		if e := m.Err(); e != nil {
+			//ignoring this error
+			continue
+		}
+		for _, k := range m.Keys() {
+			inbound := m.Get(k).(*vici.Message)
+			var ike LoadedIKE
+			if e := vici.UnmarshalMessage(inbound, &ike); e != nil {
+				//ignoring this marshal/unmarshal error!
+				continue
+			}
+			ike.Name = k
+			retVar = append(retVar, ike)
+		}
+	}
+
+	// close the socket
+	s.Close()
+
+	return retVar, nil
 }
